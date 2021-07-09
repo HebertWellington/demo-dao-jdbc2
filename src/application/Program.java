33e59cf1a8f6a0f330449861642ce8b85f270f3a
HebertWellington <hebertwellington@gmail.com>
2021-07-09 20:35:02 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1, "books");
 		System.out.println(obj);
+		
+		seller seller = new seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+		System.out.println(seller);
 	}
 
 }
