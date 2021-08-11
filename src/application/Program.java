a5a2e1706834d74b897bd99a7c8e6c5e3ad750e2
HebertWellington <hebertwellington@gmail.com>
2021-08-10 22:12:01 -0300

    insert implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.Dao.DaoFactory;
@@ -29,7 +30,12 @@ public class Program {
 		for(seller obj : list) {
 		System.out.println(obj);	
 		}
-
+		
+		System.out.println("===Test 4: seller insert===");
+		seller newSeller = new seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new id "+newSeller.getId());
+		
 
 	}
 
